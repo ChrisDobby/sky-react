858eef7cb7b23968c1db49be48f308bb3c02a04c
Chris Dobson <dobby@chrisjdobson.co.uk>
2018-09-10 08:57:54 +0100

    Removed unused file

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
 
+/* eslint-disable react/jsx-filename-extension */
 render(
     <BrowserRouter>
         <Routes />
     </BrowserRouter>,
     document.getElementById('root')
 );
+/* eslint-enable react/jsx-filename-extension */
